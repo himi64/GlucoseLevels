5e3f1bc7df106c2fe68eefd432a06f6b18bccef6
Himanshu Makharia <himi64@hotmail.com>
2017-07-04 17:29:50 -0400

    Add files via upload

--- ann_BGL.py
+++ ann_BGL.py
@@ -20,7 +20,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-### PART 1: PREDICTING DIABETIES YES OR NO (BINARY) ###########################
+### MODEL 1: PREDICTING DIABETIES YES OR NO (BINARY) ##########################
+
 ### DATA PREPROCESSING ########################################################
 
 # import dataset
@@ -117,7 +118,8 @@ Age = 45 years
 sample_patient = sc.transform(np.array([[2,100,85,30,0,35,0.8,45]]))
 sample_pred = classifier.predict(sample_patient)
 
-### PART 2: PREDICTING PLASMA GLUCOSE LEVEL ###################################
+### MODEL 2: PREDICTING PLASMA GLUCOSE LEVEL ##################################
+
 ### DATA PREPROCESSING ########################################################
 
 '''
@@ -177,6 +179,11 @@ classifier2.add(Dense(units = 8,
                      kernel_initializer = 'uniform',
                      activation = 'relu'))
 
+# add third layer
+classifier2.add(Dense(units = 8, 
+                     kernel_initializer = 'uniform',
+                     activation = 'relu'))
+
 # add output layer
 classifier2.add(Dense(units=1,
                      kernel_initializer = 'uniform',
@@ -184,28 +191,21 @@ classifier2.add(Dense(units=1,
 
 # compiling the ANN
 classifier2.compile(optimizer = 'adam', 
-                   loss = 'mean_squared_logarithmic_error', 
+                   loss = 'mean_squared_error', 
                    metrics = ['accuracy'])
 
 # fit the ANN on the training & testing set
-classifier2.fit(X2_train, y2_train, batch_size = 10, epochs = 30)
+classifier2.fit(X2_train, y2_train, batch_size = 10, epochs = 100)
 
-'''
-Best algorithm thus far: 
-    Input & hidden: relu activation
-    Output: relu or linear activation
-    Loss: MSE function or Mean squared log function
-    
+'''   
 All activation functions: https://keras.io/activations/
 All loss functions: https://keras.io/losses/
 '''
 
 # Part 3 - Making predictions and evaluating the model ########################
 
-# Predicting the Test set results (NEED TO FIGURE OUT PREDICTING UNBOUNDED 
-# AMOUNT: NEED TO UN-SCALE THE OUTPUT)
+# Predicting the Test set results
 y2_pred = classifier2.predict(X2_test) 
-# gives prediction for each observation in test set
 
 # clark error grid
 
@@ -215,6 +215,10 @@ from pylab import *
 import matplotlib.pyplot as plt
 plt.xlim(0,250)
 plt.ylim(0,250)
+plt.title('Clark Error Grid of Algorithm Accuracy')
+plt.xlabel('Actual Plasma Glucose Level')
+plt.ylabel('Predicted Plasma Glucose Level')
+
 fill([38.5,250,250,38.5], [0,0,198,32], 'b', alpha=0.2, edgecolor='r') #B
 fill([0,32,210,0], [38.5,38.5,250,250], 'b', alpha=0.2, edgecolor='r') #B
 
@@ -229,3 +233,120 @@ fill([32,187.5,32], [100,250,250], 'b', alpha=0.2, edgecolor='r') #C
 
 # insert scatter points
 clark = plt.scatter(y2_test, y2_pred)
+
+### MODEL 3: PREDICTING PLASMA GLUCOSE WITHOUT DIABETES VARIABLe###############
+
+### DATA PREPROCESSING ########################################################
+
+'''
+Dataset description:
+   1. Number of times pregnant
+   2. Diastolic blood pressure (mm Hg)
+   3. Triceps skin fold thickness (mm)
+   4. 2-Hour serum insulin (mu U/ml)
+   5. Body mass index (weight in kg/(height in m)^2)
+   6. Age (years)
+   7. Diabetes (0=no or 1=yes)
+   8. Plasma glucose concentration a 2 hours in an oral glucose tolerance test
+   
+Diabetes pedigree function is taken out 
+'''
+
+# import dataset
+data3 = pd.read_csv("diabetes3.csv")
+diabetes3 = np.array(data3) # convert to numpy array for slicing
+
+# X.dtypes
+
+X3 = diabetes3[:, 0:7]
+y3 = diabetes3[:, 7]
+
+
+# split data into training and testing datasets
+from sklearn.model_selection import train_test_split
+X3_train, X3_test, y3_train, y3_test = train_test_split(X3, y3, 
+                                                    test_size = 0.25, 
+                                                    random_state=0)
+
+# feature scaling (required for ANN)
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X3_train = sc.fit_transform(X3_train)
+X3_test = sc.fit_transform(X3_test)
+
+### BUILD THE ARTIFICIAL NEURAL NETWORK #######################################
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# initialize the ANN
+classifier3 = Sequential()
+
+# add first layer (inputs) - relu is best for unbounded
+classifier3.add(Dense(units = 7, 
+                     kernel_initializer = 'uniform', 
+                     activation = 'relu', 
+                     input_dim = 7)) # 9 nodes in the hidden layer, 19 inputs 
+                     #NB: (input nodes + output nodes) / 2 = units (# of nodes)
+
+# add second layer
+classifier3.add(Dense(units = 7, 
+                     kernel_initializer = 'uniform',
+                     activation = 'relu'))
+
+# add third layer
+classifier3.add(Dense(units = 7, 
+                     kernel_initializer = 'uniform',
+                     activation = 'relu'))
+
+# add output layer
+classifier3.add(Dense(units=1,
+                     kernel_initializer = 'uniform',
+                     activation = 'linear'))
+
+# compiling the ANN
+classifier3.compile(optimizer = 'adam', 
+                   loss = 'mean_squared_error', 
+                   metrics = ['accuracy'])
+
+# fit the ANN on the training & testing set
+classifier3.fit(X3_train, y3_train, batch_size = 10, epochs = 100)
+
+'''   
+All activation functions: https://keras.io/activations/
+All loss functions: https://keras.io/losses/
+'''
+
+# Part 3 - Making predictions and evaluating the model ########################
+
+# Predicting the Test set results
+y3_pred = classifier3.predict(X3_test) 
+
+# clark error grid
+
+# build the plot grid
+# NB: predicted values must be on the Y axis
+from pylab import *
+import matplotlib.pyplot as plt
+plt.xlim(0,250)
+plt.ylim(0,250)
+plt.title('Clark Error Grid of Algorithm Accuracy')
+plt.xlabel('Actual Plasma Glucose Level')
+plt.ylabel('Predicted Plasma Glucose Level')
+
+fill([38.5,250,250,38.5], [0,0,198,32], 'b', alpha=0.2, edgecolor='r') #B
+fill([0,32,210,0], [38.5,38.5,250,250], 'b', alpha=0.2, edgecolor='r') #B
+
+fill([135,250,250,135], [38.5,38.5,100,100], 'c', alpha=0.5, edgecolor='g') #D
+fill([0,32,32,0], [38.5,38.5,100,100], 'c', alpha=0.5, edgecolor='g') #D
+
+fill([100,250,250,100], [0,0,38.5,38.5], 'b', alpha=0.4, edgecolor='r') #E
+fill([0,32,32,0], [100,100,250,250], 'b', alpha=0.4, edgecolor='r') #E
+
+fill([80,100,100], [0,0,38.5], 'b', alpha=0.2, edgecolor='r') #C
+fill([32,187.5,32], [100,250,250], 'b', alpha=0.2, edgecolor='r') #C
+
+# insert scatter points
+clark = plt.scatter(y3_test, y3_pred)
